eb1bba6b492d7dd9bf34dd19822440abce2bf53d
WinPooh32 <winpooh32@yandex.ru>
2021-03-29 23:29:32 +1000

    Add timer

---
+++ timer.go
@@ -0,0 +1,28 @@
+package suslik
+
+import "time"
+
+type Timer struct {
+	Begin          time.Time
+	InactiveBegin  time.Time
+	ActiveDuration time.Duration
+	Run            bool
+}
+
+func (t *Timer) Start() {
+	t.ActiveDuration += t.InactiveBegin.Sub(t.Begin)
+	t.Begin = time.Now()
+	t.Run = true
+}
+
+func (t *Timer) Stop() {
+	t.InactiveBegin = time.Now()
+	t.Run = false
+}
+
+func (t *Timer) Elapsed() time.Duration {
+	if t.Run {
+		return time.Since(t.Begin) + t.ActiveDuration
+	}
+	return t.InactiveBegin.Sub(t.Begin) + t.ActiveDuration
+}
