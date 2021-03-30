766783f2e9440eeda8131c0f74bf335f156ae75e
WinPooh32 <winpooh32@yandex.ru>
2021-03-31 00:35:09 +1000

    Fix Mouse function parameters

--- responder.go
+++ responder.go
@@ -15,14 +15,14 @@ type Responder interface {
 
 type Game struct{}
 
-func (g *Game) Preload()                          {}
-func (g *Game) Setup()                            {}
-func (g *Game) Close()                            {}
-func (g *Game) Update(dt float32)                 {}
-func (g *Game) Render()                           {}
-func (g *Game) Resize(w, h float32)               {}
-func (g *Game) Mouse(x, y float32, action Action) {}
-func (g *Game) Scroll(amount float32)             {}
+func (g *Game) Preload()                                        {}
+func (g *Game) Setup()                                          {}
+func (g *Game) Close()                                          {}
+func (g *Game) Update(dt float32)                               {}
+func (g *Game) Render()                                         {}
+func (g *Game) Resize(w, h float32)                             {}
+func (g *Game) Mouse(x, y float32, button Mouse, action Action) {}
+func (g *Game) Scroll(amount float32)                           {}
 func (g *Game) Key(key Key, modifier Modifier, action Action) {
 	if key == Escape {
 		Exit()
