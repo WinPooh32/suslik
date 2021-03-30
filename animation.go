8c46616d7ffc1ebd2d9b688cd41a23a7ad4a898c
WinPooh32 <winpooh32@yandex.ru>
2021-03-30 14:08:48 +1000

    Add animation controller

---
+++ animation.go
@@ -0,0 +1,58 @@
+package suslik
+
+import (
+	"time"
+)
+
+type Animation struct {
+	TotalFrames int
+	Current     int
+	Delay       time.Duration
+	Playing     bool
+	Timer       Timer
+}
+
+func MakeAnimation(frames int, delay time.Duration) Animation {
+	return Animation{
+		TotalFrames: frames,
+		Current:     0,
+		Delay:       delay,
+		Playing:     false,
+		Timer:       MakeTimer(),
+	}
+}
+
+func (anim *Animation) Play(num int) {
+	anim.Timer.Start()
+	anim.Current = num
+	anim.Playing = true
+}
+
+func (anim *Animation) Stop(num int) {
+	anim.Timer.Stop()
+	anim.Playing = false
+}
+
+func (anim *Animation) NextFrame() int {
+	if anim.Timer.Elapsed() > anim.Delay {
+		anim.Timer.Reset()
+		anim.Current = (anim.Current + 1) % anim.TotalFrames
+	}
+	return anim.Current
+}
+
+func (anim *Animation) GetDelay() time.Duration {
+	return anim.Delay
+}
+
+func (anim *Animation) SetDelay(d time.Duration) {
+	anim.Delay = d
+}
+
+func (anim *Animation) GetTotalFrames() int {
+	return anim.TotalFrames
+}
+
+func (anim *Animation) SetTotalFrames(total int) {
+	anim.TotalFrames = total
+}
